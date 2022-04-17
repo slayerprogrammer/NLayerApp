4d3afbffd7c1128787009674cf4a3530ea01eb99
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2022-04-17 12:45:39 +0300

    IGenericRepository Added

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(long id);
+
+        //IQueryable yazmamızın sebebi ek olarak orderby veya
+        //başka sorgulamalar yapmak için daha veritabanına gitmediğimiz için.
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        //example: productRepository.Where(x=>x.Id > 5)
+        //OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
