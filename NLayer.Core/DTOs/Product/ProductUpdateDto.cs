583f0057c9d9fb57293ba77757aeb6bfecc26f08
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2022-04-26 09:17:38 +0300

    dto added and file editing completed

---
+++ NLayer.Core/DTOs/Product/ProductUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NLayer.Core.DTOs.Product
+{
+    public class ProductUpdateDto
+    {
+        public long Id{ get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
