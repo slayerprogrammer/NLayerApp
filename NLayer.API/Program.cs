3377d23e075e0e49794426df79f1c4ba3675cb6f
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2022-05-15 20:54:34 +0300

    AutoMapper and Dto completed

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Repositories;
+using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,7 +22,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 //service katmanında class ımızı oluşturmadığımız için comment ledik 
-//builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
+builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
+
 builder.Services.AddDbContext<AppDbContext>(x=>
 {
     x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),option =>
---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs.Category;
+using NLayer.Core.DTOs.Product;
+using NLayer.Core.DTOs.ProductFeature;
+using NLayer.Core.Models;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            //CreateMap metodu ile entity ve dtoları eşleştirip mapliyoruz
+            //ReversMap komutu ise bu mappingin iki yönlü olduğunu bildiriyor.
+
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+
+        }
+    }
+}
